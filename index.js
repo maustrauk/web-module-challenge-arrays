2c9207b960a8c2f5096584e9c177d76d0fda5459
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-06 15:35:49 -0400

    Task 5 to 6

--- index.js
+++ index.js
@@ -135,10 +135,11 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(userArray, ix){
+    return userArray[ix];
 }
 
+console.log(getFlavorByIndex(newArray,5));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, 
@@ -156,8 +157,8 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() and .indexOf() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(userArray,stringRemove){
+    
 }
 
 
